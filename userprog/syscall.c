859f698e685c14a42335b619bc6624f7841bb9f3
JJong-Min <sambujakkk@naver.com>
2021-10-09 22:33:04 +0900

    halt() 구현

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,11 +1,17 @@
-#include "userprog/syscall.h"
-#include <stdio.h>
-#include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/loader.h"
-#include "userprog/gdt.h"
+#include "threads/palloc.h"
 #include "threads/flags.h"
+#include "threads/vaddr.h"
+#include "userprog/gdt.h"
+#include "userprog/process.h"
+#include "userprog/syscall.h"
+#include "filesys/filesys.h"
+#include "filesys/file.h"
+#include <list.h>
+#include <stdio.h>
+#include <syscall-nr.h>
 #include "intrinsic.h"
 
 void syscall_entry (void);
@@ -44,3 +50,12 @@ syscall_handler (struct intr_frame *f UNUSED) {
 	printf ("system call!\n");
 	thread_exit ();
 }
+
+// Project 2-2. syscalls
+
+// Terminates Pintos by calling power_off(). No return.
+void halt(void)
+{
+	power_off();
+}
+
