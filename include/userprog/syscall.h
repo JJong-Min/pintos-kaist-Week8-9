d414fdc2f6a4c85dee5232eeb185fbf612efee6a
JJong-Min <sambujakkk@naver.com>
2021-10-14 02:02:16 +0900

    lock 변수 추가

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -2,5 +2,6 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
-
+// Project 2-4. File descriptor
+struct lock file_rw_lock; // prevent simultaneous read, write (race condition prevention?)
 #endif /* userprog/syscall.h */
